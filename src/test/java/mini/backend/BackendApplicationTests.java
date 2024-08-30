10b2ede399f7e59f15627e24b5eb8611dc313567
HyewonBae <43161096+hyewonbae@users.noreply.github.com>
2024-08-30 17:36:43 +0900

    init: init project

---
+++ src/test/java/mini/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mini.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
