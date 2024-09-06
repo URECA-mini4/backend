88dce616b2e72f67d15e099e4b20b6b58ae16a9e
Peindre La Colline <158552381+PeindreLaColline@users.noreply.github.com>
2024-09-06 13:22:25 +0900

    Feat#9 modify jpa auditing (#10)
    * feat#5 create comment interface
    
    * refact#5 CommentService interface
    
    commentService 인터페이스 반환값 및 메서드 명 변경
    
    * feat#5 create CommentDtoRes/Req
    
    commentDtoRes 및 Req 추가
    
    * refact#5 CommentDto/Req, Comment Service
    
    피드백 사항 반영해서 수정했습니다
    CommentDtoReq에 주석 참고해주시면 감사하겠습니다
    
    * feat: add JPA Auditing
    
    createdDate, updatedDate 변수 자동으로 값 넣어주기 위해 JPA Auditing 추가했습니다
    entity 클래스 3개 변경됐고, BackendApplication에서 어노테이션 추가됐습니다
    
    * Delete src/main/java/mini/backend/comment/CommentDtoReq.java
    
    죄송합니다 잘못 커밋했습니다
    
    * docs: delete wrong commit
    
    방금 전 커밋할 때 관련 없는 파일들 올린 거 지우고 다시 커밋합니다
    
    * refact: create BaseTimeEntity class, extends it in other classes
    
    Auditing 추가했던 거 각각 클래스에 하나씩 달았던 것들 추상 클래스 BaseTimeEntity 만들어서 그 클래스를 상속하도록 수정했습니다

--- src/main/java/mini/backend/domain/User.java
+++ src/main/java/mini/backend/domain/User.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -13,7 +14,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class User {
+public class User extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,8 +23,6 @@ public class User {
     private String id;
     private String password;
     private String name;
-    private Timestamp createdDate;
-    private Timestamp updatedDate;
 
     @Enumerated(EnumType.STRING)
     private UserStatus status; //[ACTIVED, SUSPENDED, DELETED]
