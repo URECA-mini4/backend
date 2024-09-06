bcc283916819bb12a6f0ca7c4dba7f472e602685
hyewonbae <bhwn98@gmail.com>
2024-09-06 14:08:46 +0900

    refact: refactoring post interface dto
    UserDtoRes 생성

--- src/main/java/mini/backend/post/PostBaseDtoRes.java
+++ src/main/java/mini/backend/post/PostBaseDtoRes.java
@@ -1,12 +1,15 @@
 package mini.backend.post;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import mini.backend.user.UserDtoRes;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class PostBaseDtoRes {
     private final Long postId;
-    private final Long userId;
     private final String title;
     private final boolean isAnnounce;
+    private final List<UserDtoRes> userInfo;
 }
--- src/main/java/mini/backend/post/PostDetailDtoRes.java
+++ src/main/java/mini/backend/post/PostDetailDtoRes.java
@@ -1,15 +1,18 @@
 package mini.backend.post;
 
 import lombok.*;
+import mini.backend.user.UserDtoRes;
 
-@Getter
-@ToString
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class PostDetailDtoRes extends PostBaseDtoRes{
     private final String content;
 //    private final List<CommentDtoRes> comments;
 
-    public PostDetailDtoRes(Long postId, Long userId, String title, boolean isAnnounce, String content /*,List<CommentDtoRes> comments */) {
-        super(postId, userId, title, isAnnounce);
+    public PostDetailDtoRes(Long postId, String title, boolean isAnnounce, List<UserDtoRes> userInfo, String content /*,List<CommentDtoRes> comments */) {
+        super(postId, title, isAnnounce, userInfo);
         this.content = content;
 //        this.comments = comments;
     }
