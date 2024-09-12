aa76a2ea6797d1c6c49c079aedbe64efe6106cdd
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2024-09-12 15:29:13 +0900

    fact: add updateComment, deleteComment in CommentController (#21)
    컨트롤러에 메서드 추가

--- src/main/java/mini/backend/comment/CommentDtoReq.java
+++ src/main/java/mini/backend/comment/CommentDtoReq.java
@@ -2,6 +2,7 @@ package mini.backend.comment;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import mini.backend.domain.Post;
 import mini.backend.domain.User;
 import mini.backend.user.UserDtoRes;
@@ -9,7 +10,9 @@ import mini.backend.user.UserDtoRes;
 import java.sql.Timestamp;
 
 @Getter
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class CommentDtoReq {
     private final String content;
+    private Long postId;
 }
