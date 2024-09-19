4ee4c9fcf371c9be4b397f27bdb51668ed075874
ChoiJiHooon <prin81208@naver.com>
2024-09-19 15:41:39 +0900

    fix: fix signuprequest to use annotation

--- src/main/java/mini/backend/user/SignupRequest.java
+++ src/main/java/mini/backend/user/SignupRequest.java
@@ -1,32 +1,15 @@
 package mini.backend.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+
 public class SignupRequest {
     private String id;
     private String password;
     private String name;
-
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/mini/backend/user/UserDtoRes.java
+++ src/main/java/mini/backend/user/UserDtoRes.java
@@ -9,5 +9,4 @@ public class UserDtoRes {
     private final Long userId;
     private final String id;
     private final String name;
-
 }
